e5b3da7b24ed592649599f1f8d4bc9a1e4c57ba5
anshit0523 <fernnadosaranajr@gmail.com>
2026-09-23 10:07:52 +0800

    fav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,25 +16,27 @@
 
 
     <!-- Favicon -->
-    <link
-        rel="icon"
-        type="image/x-icon"
-        href="{{ asset('favicon.ico') }}"
-    >
+ <!-- Favicon -->
+<link rel="icon" href="{{ asset('favicon.ico') }}">
 
-    <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="{{ asset('images/favicon-32x32.png') }}"
-    >
+<link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="{{ asset('images/favicon-32x32.png') }}"
+>
 
-    <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="{{ asset('images/favicon-16x16.png') }}"
-    >
+<link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="{{ asset('images/favicon-16x16.png') }}"
+>
+
+<link
+    rel="apple-touch-icon"
+    href="{{ asset('images/favicon-32x32.png') }}"
+>
 
     <!-- Preload Hero Assets -->
 <link
